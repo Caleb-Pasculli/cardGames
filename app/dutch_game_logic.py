a59c9c176f6f0d48dd9cfee032af943551d92dc5
Caleb <you@example.com>
2026-03-09 17:38:22 +1030

    git is just cloud storage

--- app/dutch_game_logic.py
+++ app/dutch_game_logic.py
@@ -8,26 +8,33 @@ class Card(BaseModel):
     value: int = None
     is_revealed: bool = False
 
+
 class Player(BaseModel):
     player_number: int
+    first_turn: bool = True
+    name: str
     id: str
     hand: list[Card]
+    picked_up_card: Card | None = None
+    special_card_played: str | None = None
+    special_activated: bool = False
+
 
 class Game(BaseModel):
     deck: list[Card]
     discard_pile: list[Card]
-    picked_up_card: Card | None = None
-
+    
     max_players: int = 2
     players: list[Player]
     players_dict: dict[str, Player]
-    current_turn: int = 0
+    current_turn: int = 1
 
     status: str
+    can_match: bool = True
+    jack_viewed: bool = False
     dutch_called: bool = False
     dutch_called_by: str | None = None
     turns_remaining: int | None = None
-
 
 
 def create_deck():
@@ -52,13 +59,14 @@ def create_deck():
     return deck
 
 
-
 def deal_cards(game: Game):
     for player in game.players:
-        for i in range(4):
+        for i in range(2):
             player.hand.append(game.deck.pop(-1))
+        for i in range(2):
+            player.hand.append(game.deck.pop(-1))
+            player.hand[-1].is_revealed = True
         game.players_dict[player.id] = player
-
 
 
 def start_game_logic(game: Game):
@@ -66,7 +74,6 @@ def start_game_logic(game: Game):
     game.discard_pile[-1].is_revealed = True
     deal_cards(game)
     game.status = "playing"
-
 
 
 def turn_validation(game: Game, player_id: str):
@@ -79,90 +86,197 @@ def turn_validation(game: Game, player_id: str):
         raise Exception("Game is not active")
 
 
-def reveal_initial_cards(player: Player):
-    player.hand[2].is_revealed = True
-    player.hand[3].is_revealed = True
-
-
-
-def hide_initial_cards(player: Player):
-    player.hand[2].is_revealed = False
-    player.hand[3].is_revealed = False
-
-
-
-def pick_up_from_deck(game: Game, player_id: int):
+def pick_up_from_deck(game: Game, player_id: str):
     turn_validation(game, player_id)
 
-    if game.picked_up_card != None:
+    current_player = game.players[game.current_turn]
+
+    if current_player.picked_up_card != None:
         raise Exception("Already picked up")
     
+
     pickedUpCard = game.deck.pop()
     pickedUpCard.is_revealed = True
-    game.picked_up_card = pickedUpCard
+    current_player.picked_up_card = pickedUpCard
+
+    if (current_player.first_turn == True):
+        for card in current_player.hand:
+            card.is_revealed = False
+        game.players[game.current_turn].first_turn = False
+
+    if(pickedUpCard.rank == "10"):
+        current_player.special_card_played = '10'
+    
+    game.can_match = False
     return pickedUpCard
 
 
 
-def pick_up_from_discard(game: Game, player_id: int):
-
+def pick_up_from_discard(game: Game, player_id: str):
     turn_validation(game, player_id)
-
+    current_player = game.players[game.current_turn]
     if not game.discard_pile:
         raise Exception("Discard Empty")
     
-    if game.picked_up_card != None:
+    if current_player.picked_up_card != None:
         raise Exception("Already picked up")
-    
+
     pickedUpCard = game.discard_pile.pop()
     pickedUpCard.is_revealed = True
-    game.picked_up_card = pickedUpCard
+    current_player.picked_up_card = pickedUpCard
+    current_player = game.players[game.current_turn]
+    if (current_player.first_turn == True):
+        current_player.hand[-1].is_revealed = False
+        current_player.hand[-2].is_revealed = False
+        game.players[game.current_turn].first_turn = False
+
+    game.can_match = False
     return pickedUpCard
 
 
-
-def discard_from_hand(player_id: int, hand_index: int, game: Game):
-
+def play_10(game: Game, player_id: str, target_player_id: str):
     turn_validation(game, player_id)
 
+    player = game.players_dict[player_id]
+    target_player = game.players_dict[target_player_id]
+    if player.picked_up_card.rank != "10":
+        raise Exception("You do not have a 10")
+    
+    player.picked_up_card.is_revealed = False
+    target_player.hand.append(player.picked_up_card)
+    player.picked_up_card = None
+
+    advance_turn(game)
+    return
+
+
+def discard_from_hand(player_id: str, hand_index: int, game: Game):
+    turn_validation(game, player_id)
+    current_player = game.players[game.current_turn]
     hand = None
     for player in game.players:
         if player.id == player_id:
             hand = player.hand
-    
+
     game.discard_pile.append(hand[hand_index])
     game.discard_pile[-1].is_revealed = True
-    hand[hand_index] = game.picked_up_card
-    game.picked_up_card = None
+    hand[hand_index] = current_player.picked_up_card
+    hand[hand_index].is_revealed = False
+    current_player.picked_up_card = None
+
+    if (game.discard_pile[-1].rank == "Jack"):
+        game.players_dict[player_id].special_activated = True
+        game.players_dict[player_id].special_card_played = "Jack"
+        return
+
+    if (game.discard_pile[-1].rank == "Queen"):
+        game.players_dict[player_id].special_activated = True
+        game.players_dict[player_id].special_card_played = "Queen"
+        return
+
     advance_turn(game)
     return
 
 
-
-def discard_pick_up_card(player_id: int, game: Game):
+def discard_pick_up_card(player_id: str, game: Game):
     turn_validation(game, player_id)
-
-    game.discard_pile.append(game.picked_up_card)
+    current_player = game.players[game.current_turn]
+    game.discard_pile.append(current_player.picked_up_card)
     game.discard_pile[-1].is_revealed = True
-    game.picked_up_card = None
+    current_player.picked_up_card = None
+
+    if (game.discard_pile[-1].rank == "Jack"):
+        game.players_dict[player_id].special_activated = True
+        game.players_dict[player_id].special_card_played = "Jack"
+        return
+    
+    if (game.discard_pile[-1].rank == "Queen"):
+        game.players_dict[player_id].special_activated = True
+        game.players_dict[player_id].special_card_played = "Queen"
+        return
+    
     advance_turn(game)
     return
 
+
+def jack_played(player_index: int, card_index: int, player_id: str, game: Game):
+    turn_validation(game, player_id)
+    if (game.players_dict[player_id].special_activated == False or 
+        game.players_dict[player_id].special_card_played != "Jack" or
+        game.jack_viewed):
+        return
+    
+    player = game.players[player_index]
+
+    revealed_card = player.hand[card_index].model_copy()
+    revealed_card.is_revealed = True
+    game.jack_viewed = True
+    return revealed_card
+
+
+def end_viewing(player_id: str, game: Game):
+    turn_validation(game, player_id)
+    
+    for player in game.players:
+        player.picked_up_card = None
+        for card in player.hand:
+            card.is_revealed = False
+
+    game.jack_viewed = False
+    advance_turn(game)
+    return
+
+
+def queen_played(player1_index: int, card1_index: int, player2_index: int, card2_index: int, game: Game, player_id: str):
+    if (game.players_dict[player_id].special_activated == False or 
+        game.players_dict[player_id].special_card_played != "Queen"):
+        return
+    turn_validation(game, player_id)
+    player1 = game.players[player1_index]
+    player2 = game.players[player2_index]
+
+    temp = player1.hand[card1_index]
+    player1.hand[card1_index] = player2.hand[card2_index]
+    player2.hand[card2_index] = temp
+
+    for card in player1.hand:
+        card.is_revealed = False
+    for card in player2.hand:
+        card.is_revealed = False
+    advance_turn(game)
+    return
+
+def match(game: Game, player_id: str, card_index: int):
+    if(not game.can_match):
+        return
+
+    current_player = game.players_dict[player_id]
+    if current_player.hand[card_index].rank != game.discard_pile[-1].rank:
+        current_player.hand.append(game.deck.pop())
+        return
+    
+    game.discard_pile.append(current_player.hand.pop(card_index))
+    game.discard_pile[-1].is_revealed = True
+    game.can_match = False
+    return
 
 
 def advance_turn(game: Game):
+    for player in game.players:
+        player.special_activated = False
+        player.special_card_played = None
+
     if game.dutch_called_by != None:
         game.turns_remaining -= 1
         if game.turns_remaining == 0:
             game.status = "finished"
             return
-
+        
+    game.can_match = True
     game.current_turn = (game.current_turn + 1) % len(game.players)
 
 
-
 def call_dutch_logic(game: Game, player_id: str):
-    
     turn_validation(game, player_id)
 
     if game.dutch_called:
@@ -174,19 +288,29 @@ def call_dutch_logic(game: Game, player_id: str):
     return
 
 
-
-def determine_winner(game: Game):
-    lowest_score_id = []
+def determine_scores(game: Game):
+    game_tied = False
+    scores = []
+    winners_name = []
+    hands: list[list[Card]] = []
     lowest_score = len(game.players[0].hand) * 20
     
     for player in game.players:
         score = 0
+        hands.append(player.hand)
         for card in player.hand:
             score += card.value
-        
-        if score < lowest_score:
-            lowest_score_id = [player.id]
-        if score == lowest_score:
-            lowest_score_id.append(player.id)
+        scores.append(score)
 
-    return lowest_score_id, lowest_score
+        if score < lowest_score:
+            winners_name = [player.name]
+            lowest_score = score
+        elif score == lowest_score:
+            winners_name.append(player.name)
+
+    for hand in hands:
+        for card in hand:
+            card.is_revealed = True
+
+    if len(winners_name) > 1: game_tied = True
+    return winners_name, scores, hands, game_tied
--- app/routes/dutch.py
+++ app/routes/dutch.py
@@ -11,17 +11,17 @@ router = APIRouter(prefix="/dutch")
 games: dict[str, Game] = {}
 
 @router.post("/create")
-def create_game(max_players: int = Body(..., embed=True)):
+def create_game(max_players: int = Body(..., embed=True), name: str = Body(..., embed=True)):
     player_number = 0
     player_id = str(uuid.uuid4())
-    players = [Player(player_number = player_number, id = player_id, hand=[])]
+    players = [Player(name = name, player_number = player_number, id = player_id, hand=[])]
 
     game = Game(
         deck = create_deck(),
         discard_pile=[],
         max_players=max_players,
         players=players,
-        players_dict= {player_id: Player(player_number = player_number, id = player_id, hand=[])},
+        players_dict= {player_id: Player(name = name, player_number = player_number, id = player_id, hand=[])},
         status="waiting"
     )
 
@@ -36,7 +36,7 @@ def create_game(max_players: int = Body(..., embed=True)):
 
 
 @router.post("/{game_id}/join")
-def join_game(game_id: str):
+def join_game(game_id: str, name: str = Body(..., embed=True)):
     if game_id not in games:
         raise HTTPException("Game not found")
     if games[game_id].status != "waiting":
@@ -47,7 +47,7 @@ def join_game(game_id: str):
 
     player_number = len(game.players)
     player_id = str(uuid.uuid4())
-    player = Player(player_number = player_number, id = player_id, hand=[])
+    player = Player(name = name, player_number = player_number, id = player_id, hand=[])
     game.players.append(player)
     game.players_dict[player_id] = player
 
@@ -77,24 +77,43 @@ def get_game(game_id: str, player_id: str):
         raise HTTPException("Game not found")
     
     game = games[game_id]
-
+    opponent_name = ""
     opponent_hand_size = None
     for i in range(len(game.players)):
         if(game.players[i].id != player_id):
+            opponent_name = game.players[i].name
             opponent_hand_size = len(game.players[i].hand)
-        
+
+    current_player = game.players_dict[player_id]
+
+    player_name: str = current_player.name
+    player_picked_up_card: Card = current_player.picked_up_card
+    player_first_turn: bool = current_player.first_turn
+    first_turn_cards: list[Card] | None
+    if(player_first_turn and game.status == 'playing'):
+        first_turn_cards = [current_player.hand[-2], current_player.hand[-1]]
+    else:
+        first_turn_cards = None
 
     return {
         "game_id": game_id,
         "status": game.status,
+        "player_name": player_name,
         "player_turn": game.current_turn,
+        "player_first_turn": player_first_turn,
+        "player_first_turn_cards": first_turn_cards,
+        "player_hand_size": len(game.players_dict[player_id].hand),
+        "opponent_name": opponent_name,
         "opponent_hand_size": opponent_hand_size,
-        "hand": game.players_dict[player_id].hand,
         "discard_pile": game.discard_pile,
-        "picked_up_card": game.picked_up_card,
+        "picked_up_card": player_picked_up_card,
         "dutch_called": game.dutch_called,
         "dutch_called_by": game.dutch_called_by,
-        "turns_remaining": game.turns_remaining
+        "turns_remaining": game.turns_remaining,
+        "can_match": game.can_match,
+        "special_activated": game.players_dict[player_id].special_activated,
+        "special_card_played": game.players_dict[player_id].special_card_played,
+        "current_turn": game.current_turn
     }
 
 
@@ -137,6 +156,58 @@ def discard_picked(game_id: str, player_id: str = Body(..., embed=True)):
     except Exception as e:
         raise HTTPException(400, str(e))
 
+@router.post("/{game_id}/{player_id}/special/10")
+def used_10(game_id: str, player_id: str, target_id: str = Body(..., embed=True)):
+    game = games[game_id]
+    if target_id == "0":
+        for id in game.players_dict:
+            if id != player_id:
+                target_id = id
+                break
+    try:
+        play_10(game, player_id, target_id)
+        return
+    except Exception as e:
+        raise HTTPException(400, str(e))
+
+
+@router.post("/{game_id}/{player_id}/special/jack")
+def jack_additional_info(game_id: str, player_id: str, player_index: int = Body(..., embed=True), card_index: int = Body(..., embed=True)):
+    try:
+        game = games[game_id]
+        return jack_played(player_index, card_index, player_id, game)
+    except Exception as e:
+        raise HTTPException(400, str(e))
+
+
+@router.post("/{game_id}/{player_id}/special/queen")
+def queen_additional_info(game_id: str, player_id: str,
+                          player1_index: int = Body(..., embed=True), card1_index: int = Body(..., embed=True),
+                          player2_index: int = Body(..., embed=True), card2_index: int = Body(..., embed=True)):
+    try:
+        game = games[game_id]
+        return queen_played(player1_index, card1_index, player2_index, card2_index, game, player_id)
+    except Exception as e:
+        raise HTTPException(400, str(e))
+
+
+@router.post("/{game_id}/{player_id}/special/end")
+def end_jack_viewing(game_id: str, player_id: str):
+    try:
+        game = games[game_id]
+        end_viewing(player_id, game)
+    except Exception as e:
+        raise HTTPException(400, str(e))
+
+
+@router.post("/{game_id}/{player_id}/match")
+def match_card(game_id: str, player_id: str, card_index: int = Body(..., embed=True)):
+    try:
+        game = games[game_id]
+        return match(game, player_id, card_index)
+    except Exception as e:
+        raise HTTPException(400, str(e))
+    
 
 @router.post("/{game_id}/call-dutch")
 def call_dutch(game_id: str, player_id: str = Body(..., embed=True)):
@@ -149,16 +220,17 @@ def call_dutch(game_id: str, player_id: str = Body(..., embed=True)):
         raise HTTPException(400, str(e))
 
 
-@router.get("/{game_id}/winner")
+@router.get("/result/{game_id}")
 def get_winner(game_id: str):
     game = games[game_id]
-
     if game.status != "finished":
         raise HTTPException(400, "Game not finished")
 
-    winners, score = determine_winner(game)
+    winners, scores, hands, game_tied = determine_scores(game)
 
     return {
         "winners": winners,
-        "score": score
+        "scores": scores,
+        "hands": hands,
+        "game_tied": game_tied
     }
